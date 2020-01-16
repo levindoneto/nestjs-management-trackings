ad024e5353d665445f01f0189f8a2bcd3702a0e6
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2020-01-16 12:56:53 -0300

    Add initial project

---
+++ config/configuration.ts
@@ -0,0 +1,7 @@
+export default () => ({
+  COBRANCA_ROUTER_KEY: "key here",
+  URL_COMMANDS: "https://msging.net/commands",
+  URI_TRACK_VALOR_ACORDO: "/event-track/",
+  MAX_TICKETS: 214748364,
+  POSTMASTER: "postmaster@analytics.msging.net"
+});
